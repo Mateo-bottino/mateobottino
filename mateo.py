9ad76d493efe80ebb153eaf19559e8b064d9e2e2
Mateo-bottino <a.mateobottino@icea.edu.ar>
2024-10-01 18:10:41 +0000

    k

--- mateo.py
+++ mateo.py
@@ -1,1 +1,14 @@
-a
+a=2+2
+print("nombre")
+mientras=True
+while a<10:
+  a+=1
+  print(a)
+print("gabriel")
+numero=0
+while numero<20:
+  print(numero)
+  numero=numero + 1
+  pesos=20000
+  valordeldolar=1240
+print("cambio")
